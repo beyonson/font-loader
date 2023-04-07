3e6e8855a086c09f77eb055cccb3d91636e73995
beyonson <garrettmarcinak@gmail.com>
2023-04-07 11:55:44 -0400

    have timer working with font loader

--- ui/main.py
+++ ui/main.py
@@ -1,11 +1,60 @@
 import sys
 import os
+import subprocess
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
 from circular_progress import CircularProgress
 from ui_newnew import Ui_TypeWriter
+from ui_splash_screen import Ui_SplashScreen
+
+counter = 0
+
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        # create splash screen
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.ui = Ui_SplashScreen()
+        self.ui.setupUi(self)
+
+        # configure circular progress
+        self.progress = CircularProgress()
+        self.progress.width = 270
+        self.progress.height = 270
+        self.progress.value = 0
+        self.progress.fontSize = 30
+        self.progress.setFixedSize(self.progress.width, self.progress.height)
+        self.progress.move(15,15)
+        self.progress.addShadow(True)
+        self.progress.setParent(self.ui.centralwidget)
+        self.progress.show()
+
+        # create timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(30)
+
+        self.show()
+
+        # run font loader
+        subprocess.Popen('../build/font-loader ../fonts/DroidSansMono.ttf', shell=True)
+
+    def update(self):
+        global counter
+
+        self.progress.setValue(counter)
+
+        if counter >= 100:
+            self.timer.stop()
+            self.main = MainWindow()
+            self.main.show()
+            self.close()
+
+        counter += 1
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -23,6 +72,6 @@ class MainWindow(QMainWindow):
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = SplashScreen()
 
     sys.exit(app.exec())
